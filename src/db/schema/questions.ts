128b93b56624ddeae0cec52f39641c306d777ab1
Brendon Gomes <brendongomes40@gmail.com>
2025-07-11 21:50:29 -0300

    feat: implement CRUD operations for questions and rooms with corresponding routes and database schema

---
+++ src/db/schema/questions.ts
@@ -0,0 +1,13 @@
+import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { rooms } from "./rooms.ts";
+
+export const questions = pgTable("questions", {
+  id: uuid().primaryKey().defaultRandom(),
+  roomId: uuid("room_id")
+    .references(() => rooms.id)
+    .notNull(),
+  question: text().notNull(),
+  answer: text(),
+
+  createdAt: timestamp().defaultNow().notNull(),
+});
