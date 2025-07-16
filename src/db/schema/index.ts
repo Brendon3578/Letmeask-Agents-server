8119f94de440235f187de5a633c906a3946f9474
Brendon Gomes <brendongomes40@gmail.com>
2025-07-16 00:49:15 -0300

    feat: add audio upload and transcription functionality and auto answer for new questions
    - Introduced new route for uploading audio files and transcribing them using Gemini API.
    - Created `audio_chunks` table to store audio transcriptions and embeddings.
    - Implemented embedding generation and answer retrieval based on transcriptions.
    - Updated package dependencies to include @fastify/multipart and @google/genai.
    - Added environment variable for GEMINI_API_KEY.

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,8 +1,11 @@
 // Barrel file index.ts
+
+import { audioChunks } from "./audio-chunks.ts";
 import { questions } from "./questions.ts";
 import { rooms } from "./rooms.ts";
 
 export const schema = {
   rooms,
   questions,
+  audioChunks,
 };
